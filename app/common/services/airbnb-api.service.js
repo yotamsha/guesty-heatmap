5f196a409f166881c7add0a30aebd9309c36cc47
yotam shalev <yotamsha@gmail.com>
2017-05-20 15:26:02 +0300

    Some cleaning and comments.

--- app/common/services/airbnb-api.service.js
+++ app/common/services/airbnb-api.service.js
@@ -2,22 +2,21 @@
  * Created by yotam on 22/12/2016.
  */
 angular.module('myApp.services')
-    .service('AirbnbAPIService', ['$http','$q', function ($http, $q) {
+    .service('AirbnbAPIService', ['$http', '$q', function ($http, $q) {
         var RESULTS_LIMIT = 1000; // This is the current max Airbnb allows for this API call.
         var PAGE_LIMIT = 50;
-        var PAGES_IN_BATCH = 20;
+        var PAGES_IN_BATCH = RESULTS_LIMIT / PAGE_LIMIT;
         var API_PREFIX = 'https://api.airbnb.com/v2';
 
         function _getListingsPagesBatch(location) {
             var requests = [];
-            for (var i = 0; i < PAGES_IN_BATCH; i++){
-                requests.push(_getListingsPageByLocation(location, i*PAGE_LIMIT));
+            for (var i = 0; i < PAGES_IN_BATCH; i++) {
+                requests.push(_getListingsPageByLocation(location, i * PAGE_LIMIT));
             }
             return $q.all(requests);
         }
-        
-        function _getListingsPageByLocation(location, offset){
-            console.log("requesting with offset: "+ offset);
+
+        function _getListingsPageByLocation(location, offset) {
             return $http.get(API_PREFIX + '/search_results?' +
                 'client_id=3092nxybyb0otqw18e8nh5nty&' +
                 '_limit=' + PAGE_LIMIT + '&' +
@@ -27,22 +26,17 @@ angular.module('myApp.services')
                     return result.data.search_results;
                 });
         }
-        
-        
+
+
         var Service = {
             getListingsByLocation: function (location) {
-                return _getListingsPagesBatch(location).then(function(results){
+                return _getListingsPagesBatch(location).then(function (results) {
                     var mergedResults = [].concat.apply([], results);
                     return mergedResults
                 });
-                //var batchRequests = [];
-/*                for (var offset = 0; offset < RESULTS_LIMIT; offset+=PAGE_LIMIT){
-                    batchRequests = _getListingsPagesBatch(location, offset);
-                }*/
-                
 
             }
-            
+
         };
 
         return Service;
